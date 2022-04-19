8cd39f491a2cbc42adf770db05499333127f3486
Ryan Harvey <ryan.j.harvey@outlook.com>
2022-04-19 22:57:25 +0100

    add kafka to running plugins

--- pkg/plugins/kafka/kafka.go
+++ pkg/plugins/kafka/kafka.go
@@ -1,11 +1,40 @@
 package kafka
 
-import "github.com/confluentinc/confluent-kafka-go/kafka"
+import (
+	"context"
+	"fmt"
+	"github.com/confluentinc/confluent-kafka-go/kafka"
+	"github.com/deepfence/PacketStreamer/pkg/config"
+)
 
 type Plugin struct {
-	Producer kafka.Producer
+	Producer    *kafka.Producer
+	Topic       string
+	MessageSize int64
 }
 
-func NewPlugin() *Plugin {
-	return &Plugin{}
+func NewPlugin(config *config.KafkaPluginConfig) (*Plugin, error) {
+	producer, err := kafka.NewProducer(&kafka.ConfigMap{
+		"bootstrap.server": config.Brokers,
+		"client.id":        config.ClientId,
+		"acks":             "all",
+	})
+
+	if err != nil {
+		return nil, fmt.Errorf("error creating kafka producer, %w", err)
+	}
+
+	return &Plugin{
+		Producer:    producer,
+		Topic:       config.Topic,
+		MessageSize: int64(*config.MessageSize),
+	}, nil
+}
+
+func (p *Plugin) Start(ctx context.Context) chan<- string {
+	inputChan := make(chan string)
+	go func() {
+
+	}()
+	return inputChan
 }
