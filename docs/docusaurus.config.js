c734e3339e3a3e08a659e9d81be0ea4198c34045
Owen Garrett <owen.garrett@gmail.com>
2022-07-15 16:39:42 +0100

    github link

--- docs/docusaurus.config.js
+++ docs/docusaurus.config.js
@@ -38,7 +38,7 @@ const config = {
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/ogarrett/PacketStreamer/docs/',
+            'https://github.com/deepfence/docs/PacketStreamer/',
           breadcrumbs: true,
           routeBasePath: '/',
           // options for remark-admonitions
